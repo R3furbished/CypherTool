e1bbfc14e5844d445a57e53933a7f8c32c28d055
R3furbished <migas.2.3@outlook.pt>
2024-11-24 19:08:11 +0000

    Gui is working.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 cd ./build/
-g++ -std=c++20 ../src/main.cpp ../libs/vigenere.cpp -o vigenere
+++ -std=c++20 ../src/main.cpp ../libs/vigenere.cpp -o vigenere `../../WxWidgetsSrc/wxWidgets-3.2.6/MacBuild/wx-config --cxxflags --libs`
 cd ..
 ./build/vigenere d SEGREDO "`cat result`"
+
--- src/main.cpp
+++ src/main.cpp
@@ -2,55 +2,114 @@
 #include <functional>
 #include <thread>
 #include <vector>
+#include <wx/wx.h>
 
-int main(int argc, char *argv[]) {
-  if (argc != 4)
-    return EXIT_FAILURE;
+class VigenereUI : public wxApp {
+public:
+    virtual bool OnInit();
+};
 
-  // Take 2 inputs : key & text;
-  char *operation = argv[1];
-  std::string key = argv[2];
-  std::string text = argv[3];
-  //
-  // Create Pairs vector;
-  const VEC_OF_PAIRS vector_of_pairs = generate_pairs(key, text);
-  int size_to_mid = vector_of_pairs.size() / 2;
-  //
-  // Create Result vectors for threads;
-  std::vector<char> thread1_arr;
-  std::vector<char> thread2_arr;
-  //
-  if (*operation == 'e') {
-    // Spawn extra threads;
-    std::thread thread1(encrypt_parallel, 0, size_to_mid,
-                        std::ref(vector_of_pairs), std::ref(thread1_arr));
-    std::thread thread2(encrypt_parallel, size_to_mid, vector_of_pairs.size(),
-                        std::ref(vector_of_pairs), std::ref(thread2_arr));
-    //
-    // Join threads;
-    thread1.join();
-    thread2.join();
-  } else if (*operation == 'd') {
-    // Spawn extra threads;
-    std::thread thread1(decrypt_parallel, 0, size_to_mid,
-                        std::ref(vector_of_pairs), std::ref(thread1_arr));
-    std::thread thread2(decrypt_parallel, size_to_mid, vector_of_pairs.size(),
-                        std::ref(vector_of_pairs), std::ref(thread2_arr));
-    //
-    // Join threads;
-    thread1.join();
-    thread2.join();
-  }
-  //
-  // Agregate the result
-  std::vector<char> result;
-  result.insert(result.end(), thread1_arr.begin(), thread1_arr.end());
-  result.insert(result.end(), thread2_arr.begin(), thread2_arr.end());
-  //
-  // Publish Result
-  for(char c : result){
-    std::cout << c << " ";
-  }
-  std::cout<<"\n";
+class MyFrame : public wxFrame {
+public:
+    MyFrame(const wxString& title);
 
+private:
+    void OnEncrypt(wxCommandEvent& event);
+    void OnDecrypt(wxCommandEvent& event);
+
+    wxTextCtrl* keyInput;
+    wxTextCtrl* textInput;
+    wxTextCtrl* resultOutput;
+
+    wxDECLARE_EVENT_TABLE();
+};
+
+enum {
+    ID_Encrypt = 2,
+    ID_Decrypt = 3
+};
+
+wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
+    EVT_BUTTON(ID_Encrypt, MyFrame::OnEncrypt)
+    EVT_BUTTON(ID_Decrypt, MyFrame::OnDecrypt)
+wxEND_EVENT_TABLE()
+
+wxIMPLEMENT_APP(VigenereUI);
+
+bool VigenereUI::OnInit() {
+    MyFrame* frame = new MyFrame("Vigenere Cipher App");
+    frame->Show(true);
+    return true;
 }
+
+MyFrame::MyFrame(const wxString& title) : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(500, 400)) {
+    wxPanel* panel = new wxPanel(this, wxID_ANY);
+
+    wxBoxSizer* vbox = new wxBoxSizer(wxVERTICAL);
+
+    wxStaticText* keyLabel = new wxStaticText(panel, wxID_ANY, "Key:");
+    keyInput = new wxTextCtrl(panel, wxID_ANY);
+    wxStaticText* textLabel = new wxStaticText(panel, wxID_ANY, "Text:");
+    textInput = new wxTextCtrl(panel, wxID_ANY);
+    wxStaticText* resultLabel = new wxStaticText(panel, wxID_ANY, "Result:");
+    resultOutput = new wxTextCtrl(panel, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE | wxTE_READONLY);
+
+    wxButton* encryptButton = new wxButton(panel, ID_Encrypt, "Encrypt");
+    wxButton* decryptButton = new wxButton(panel, ID_Decrypt, "Decrypt");
+
+    vbox->Add(keyLabel, 0, wxEXPAND | wxALL, 10);
+    vbox->Add(keyInput, 0, wxEXPAND | wxALL, 10);
+    vbox->Add(textLabel, 0, wxEXPAND | wxALL, 10);
+    vbox->Add(textInput, 0, wxEXPAND | wxALL, 10);
+    vbox->Add(resultLabel, 0, wxEXPAND | wxALL, 10);
+    vbox->Add(resultOutput, 1, wxEXPAND | wxALL, 10);
+    vbox->Add(encryptButton, 0, wxALL, 10);
+    vbox->Add(decryptButton, 0, wxALL, 10);
+
+    panel->SetSizer(vbox);
+}
+
+void MyFrame::OnEncrypt(wxCommandEvent& event) {
+    std::string key = std::string(keyInput->GetValue().mb_str());
+    std::string text = std::string(textInput->GetValue().mb_str());
+
+    // Create pairs and process text
+    VEC_OF_PAIRS pairs = generate_pairs(key, text);
+    int mid = pairs.size() / 2;
+
+    std::vector<char> thread1Result, thread2Result;
+    std::thread t1(encrypt_parallel, 0, mid, std::ref(pairs), std::ref(thread1Result));
+    std::thread t2(encrypt_parallel, mid, pairs.size(), std::ref(pairs), std::ref(thread2Result));
+    t1.join();
+    t2.join();
+
+    // Combine results
+    std::vector<char> result(thread1Result.begin(), thread1Result.end());
+    result.insert(result.end(), thread2Result.begin(), thread2Result.end());
+
+    // Display the result
+    resultOutput->SetValue(wxString(std::string(result.begin(), result.end())));
+}
+
+void MyFrame::OnDecrypt(wxCommandEvent& event) {
+    std::string key = std::string(keyInput->GetValue().mb_str());
+    std::string text = std::string(textInput->GetValue().mb_str());
+
+    // Create pairs and process text
+    VEC_OF_PAIRS pairs = generate_pairs(key, text);
+    int mid = pairs.size() / 2;
+
+    std::vector<char> thread1Result, thread2Result;
+    std::thread t1(decrypt_parallel, 0, mid, std::ref(pairs), std::ref(thread1Result));
+    std::thread t2(decrypt_parallel, mid, pairs.size(), std::ref(pairs), std::ref(thread2Result));
+    t1.join();
+    t2.join();
+
+    // Combine results
+    std::vector<char> result(thread1Result.begin(), thread1Result.end());
+    result.insert(result.end(), thread2Result.begin(), thread2Result.end());
+
+    // Display the result
+    resultOutput->SetValue(wxString(std::string(result.begin(), result.end())));
+}
+
